0eba57ba056724e0a7c889d6a5cfb0ebdaeee502
mattamu <104015477+mattamu@users.noreply.github.com>
2022-06-07 19:52:56 +0300

    Update get-review.py

--- functions/get-review.py
+++ functions/get-review.py
@@ -4,10 +4,10 @@ from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 def main(dict):
     # my IAM_API_KEY
-    authenticator = IAMAuthenticator("")
+    authenticator = IAMAuthenticator("SzJ5tNAzMZS3OpYTLkzOAxaBU-0lckN8L-4WF9EFmAT2")
     service = CloudantV1(authenticator=authenticator)
     # my COUCH_URL
-    service.set_service_url("")
+    service.set_service_url("https://722b0a8f.eu-gb.apigw.appdomain.cloud/capstone")
     response = service.post_find(
         db='reviews',
         selector={'dealership': {'$eq': int(dict["id"])}},
