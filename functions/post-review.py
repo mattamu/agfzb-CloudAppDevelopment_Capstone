6fb0998f1ab53cb64577be412d7db3da5cb25783
mattamu <104015477+mattamu@users.noreply.github.com>
2022-06-07 19:19:47 +0300

    Update post-review.py

--- functions/post-review.py
+++ functions/post-review.py
@@ -1,22 +1,4 @@
-# IBM Action, method=GET, Python 3.9
-# API https://eca74085.eu-gb.apigw.appdomain.cloud/postreviews
-# Params
-# {
-# "review": 
-#     {
-#         "id": 1114,
-#         "name": "Upkar Lidder",
-#         "dealership": 15,
-#         "review": "Great service!",
-#         "purchase": false,
-#         "another": "field",
-#         "purchase_date": "02/16/2021",
-#         "car_make": "Audi",
-#         "car_model": "Car",
-#         "car_year": 2021
-#     }
-# }
-# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 
 import sys
 from ibmcloudant.cloudant_v1 import CloudantV1
@@ -27,10 +9,10 @@ from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 def main(dict):
     # my IAM_API_KEY
-    authenticator = IAMAuthenticator("CWo8t7mAGKyhAJN2AxMmBOj6DqJTYg1ndXhYPaZfZiXM")
+    authenticator = IAMAuthenticator("")
     service = CloudantV1(authenticator=authenticator)
     # my COUCH_URL
-    service.set_service_url("https://1708aa9a-50ab-4ff8-9c91-61c949d17ed2-bluemix.cloudantnosqldb.appdomain.cloud")
+    service.set_service_url("")
     response = service.post_document(db='reviews', document=dict["review"]).get_result()
     try:
     # result_by_filter=my_database.get_query_result(selector,raw_result=True)
